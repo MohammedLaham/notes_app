50d0c270aa8277baf531a4e1dbfd2e5119043a58
MohammedLaham <prog.khan.2002@gmail.com>
2023-08-13 01:01:17 +0300

    change add note bottom sheetloading

--- lib/views/add_note_modelsheet.dart
+++ lib/views/add_note_modelsheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_state.dart';
 import 'package:note_app/views/add_note_form.dart';
@@ -17,23 +16,24 @@ class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
 
   @override
   Widget build(BuildContext context) {
-    return    BlocProvider(create: (context) => AddNoteCubit(),
-    child:  Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: BlocConsumer<AddNoteCubit, AddNoteState>(
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child:
+          BlocConsumer<AddNoteCubit, AddNoteState>(
               builder: (context, state) {
-                return ModalProgressHUD(
-                    inAsyncCall: state is AddNoteLoading ? true : false,
-                    child: const SingleChildScrollView(child: AddNoteForm()));
-              },
-              listener: (context, state) {
-                if (state is AddNoteFailure) {
-
-                }
-                if (state is AddNoteSuccess) {
-                  Navigator.pop(context);
-                }
-              })),
+        return  AbsorbPointer(
+          //عشان تمنعك من عمل اي اشي ع السكرين في حال كان بيحمل
+          absorbing: state is AddNoteLoading ? true : false ,
+          child:const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: SingleChildScrollView(child: AddNoteForm())),
+        );
+      }, listener: (context, state) {
+        if (state is AddNoteFailure) {}
+        if (state is AddNoteSuccess) {
+          Navigator.pop(context);
+        }
+      }),
     );
   }
 }
--- lib/views/custom_button.dart
+++ lib/views/custom_button.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/constants.dart';
 
+// ignore: must_be_immutable
 class CustomButton extends StatelessWidget {
-  const CustomButton({Key? key,this.onTap}) : super(key: key);
-  final void Function() ?onTap;
+  CustomButton({Key? key, this.onTap, this.isLoading = false})
+      : super(key: key);
+  final void Function()? onTap;
+  bool isLoading;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-      //من خلالها بقدر اصل لاي اشي اله علاقة بسكرين الجهاز
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-          color: kPrimaryColor, borderRadius: BorderRadius.circular(8)),
-      height: 50,
-      child: const Center(
-        child: Text(
-          'Add',
-          style: TextStyle(
-              color: Colors.black,
-              fontSize: 18,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.bold),
+        //من خلالها بقدر اصل لاي اشي اله علاقة بسكرين الجهاز
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            color: kPrimaryColor, borderRadius: BorderRadius.circular(8)),
+        height: 50,
+        child: Center(
+          child: isLoading
+              ? const SizedBox(
+            height: 24,
+                  width: 24,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                )
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.bold),
+                ),
         ),
       ),
-    ),);
+    );
   }
 }
