7670a9dba87fbfa33ce1825a45f44d64bf263de7
MohammedLaham <prog.khan.2002@gmail.com>
2023-08-14 23:57:50 +0300

    create fetch notes method

---
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:note_app/constants.dart';
+import 'package:note_app/cubits/notes_cubit/notes_state.dart';
+import 'package:note_app/models/note_model.dart';
+
+class NotesCubit extends Cubit<NotesState>{
+  NotesCubit():super(InitialNotes());
+  fetchAllNotes()async{
+    try{
+      var notesBox=Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> list=notesBox.values.toList();
+      emit(SuccessNotes(list));
+    }catch(e){
+      emit(FauilerNotes(e.toString()));
+    }
+}
+}
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:note_app/views/add_note_modelsheet.dart';
 import 'package:note_app/views/notes_view_body.dart';
 
@@ -7,23 +9,26 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //تعبر عن ارتفاع الكيبورد ويعني انه انا مش محتاج السكرين تغير من حجمها عشان تناسب الكيبورد لما تظهر
-    //  resizeToAvoidBottomInset: false,
-      body: const NotesViewBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16)),
-              context: context,
-              builder: (context) {
-                return const AddNoteBottomSheet();
-              });
-        },
-        child: const Icon(
-          Icons.add,
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        //تعبر عن ارتفاع الكيبورد ويعني انه انا مش محتاج السكرين تغير من حجمها عشان تناسب الكيبورد لما تظهر
+        //  resizeToAvoidBottomInset: false,
+        body: const NotesViewBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)),
+                context: context,
+                builder: (context) {
+                  return const AddNoteBottomSheet();
+                });
+          },
+          child: const Icon(
+            Icons.add,
+          ),
         ),
       ),
     );
